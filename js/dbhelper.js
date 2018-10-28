78c370794d1e0e891f692e21813c3be7c3db276c
Philliph Drummond <oilsinwater@gmail.com>
2018-10-28 15:45:32 -0700

    feat: updates dbhelper and js files with asignments

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 1337; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants`;
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
